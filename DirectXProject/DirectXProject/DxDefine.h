9f64f7b29cf9d1dd9fb7172327772b9313a66550
NemoTaek <vkfkdchfhr@gmail.com>
2023-02-28 21:29:07 +0900

    directX initialization
    directX initialization

--- DirectXProject/DirectXProject/DxDefine.h
+++ DirectXProject/DirectXProject/DxDefine.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+using namespace DirectX;
+
+// warning C4316 처리용 include
+// warning C4316: 'D3DClass': 힙에 할당된 개체가 16에 맞추어져 있지 않을 수도 있습니다.
+// XMMATRIX 정의에 __declspec(align(16))으로 16바이트 정렬을 사용한다.
+// 여기서 힙에 할당할 때 경고가 발생한다.
+// Visual Studio에 내장되어 사용되는 new 연산자의 경우 8byte align으로 할당되므로 16byte align이 되도록 새로 만들어 사용 가능하도록 보장해주어야 한다.
+// 이를 포함하지 않으면 CPU에서 SSE 명령어 연산을 처리할 때 정렬되는 데이터를 안전하게 보장받을 수 없다.
+#include "AlignedAllocationPolicy.h"
--- DirectXProject/DirectXProject/GraphicsClass.cpp
+++ DirectXProject/DirectXProject/GraphicsClass.cpp
@@ -1,5 +1,8 @@
 #include "Stdafx.h"
 #include "GraphicsClass.h"
+#include "D3DClass.h"
+#include <iostream>
+using namespace std;
 
 GraphicsClass::GraphicsClass() {}
 GraphicsClass::GraphicsClass(const GraphicsClass& other) {}
@@ -7,20 +10,54 @@ GraphicsClass::~GraphicsClass() {}
 
 bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 {
+	// Direct3D 객체 생성
+	m_Direct3D = (D3DClass*)_aligned_malloc(sizeof(D3DClass), 16);
+	if (!m_Direct3D) return false;
+
+	// Direct3D 객체 초기화
+	if (!m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR)) {
+		MessageBox(hwnd, L"Could not initialize Direct3D", L"Error", MB_OK);
+		return false;
+	}
+
+	FILE* fp = nullptr;
+	fopen_s(&fp, "비디오 카드 정보.txt", "wt");
+	if (fp != nullptr)
+	{
+		char cardName[255] = {};
+		int memory = 0;
+		m_Direct3D->GetVideoCardInfo(cardName, memory);
+		fprintf_s(fp, "그래픽 카드 이름: %s \n", cardName);
+		fprintf_s(fp, "그래픽 카드 메모리 크기: %d", memory);
+		fclose(fp);
+	}
+
 	return true;
 }
 
 void GraphicsClass::Shutdown()
 {
-
+	// Direct3D 객체 반환
+	if (m_Direct3D) {
+		m_Direct3D->Shutdown();
+		_aligned_free(m_Direct3D);
+		m_Direct3D = 0;
+	}
 }
 
 bool GraphicsClass::Frame()
 {
-	return true;
+	// 그래픽 렌더링 처리
+	return Render();
 }
 
 bool GraphicsClass::Render()
 {
+	// Scene을 그리기 위해 버퍼 삭제
+	m_Direct3D->BeginScene(1.0f, 1.0f, 0.0f, 1.0f);
+
+	// 버퍼의 내용을 화면에 출력
+	m_Direct3D->EndScene();
+
 	return true;
 }
